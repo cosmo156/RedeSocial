12c5d068638b5c3a3fd06fa4a492ad931344b5a3
Ismael Lima <130599536+Ismaellima4@users.noreply.github.com>
2023-11-08 11:41:35 -0300

    Delete redeSocialJavaPoo/src/main/java/br/edu/ifpb/poo/interfaces directory

--- redeSocialJavaPoo/src/main/java/br/edu/ifpb/poo/interfaces/ComentarioIF.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.poo.interfaces;
-
-import br.edu.ifpb.poo.domain.Usuario;
-
-public interface ComentarioIF{
-
-    Usuario getAutor();
-    String getTexto();
-}
--- redeSocialJavaPoo/src/main/java/br/edu/ifpb/poo/interfaces/CurtidaIF.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpb.poo.interfaces;
-
-public interface CurtidaIF {
-
-    int getNumeroCurtidas();
-    void curtir();
-    void descurtir();
-}
