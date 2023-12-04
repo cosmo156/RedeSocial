0a5ebadba303b1aebad38b9040fab9438be67cd3
Ismael Lima <130599536+Ismaellima4@users.noreply.github.com>
2023-12-04 10:45:26 -0300

    Update Adpter

--- redeSocialJavaPooRefatorada/src/main/java/br/edu/ifpb/poo/adpter/DataService.java
+++ redeSocialJavaPooRefatorada/src/main/java/br/edu/ifpb/poo/adpter/DataService.java
@@ -8,16 +8,14 @@ import java.util.List;
 public interface DataService {
     void add(Usuario user);
     List<Usuario> getAllUser();
-    void updateUser(Usuario user);
     Usuario searchUser(String nome);
     boolean userExists(String nome);
-    void removeUser(Usuario user);
     Usuario logarUsuario(String nome, String senha);
     void fazerPostagem(Usuario user, Postagem postagem);
     void fazerComentario(Usuario user, Postagem postagem, Comentario comentario);
     void curtir(Usuario user, Postagem postagem);
     void removerCurtida(Usuario user, Postagem postagem);
-
+    List<Postagem> getAllPost();
     List<Postagem> getPostagens(Usuario user);
     List<Comentario> getComentarios(Postagem postagem);
     int getCurtidas(Postagem postagem);
